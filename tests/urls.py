6db501984ea628e4a18f88dee1c68f51ea4f55fa
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-08-13 11:39:51 -0400

    Use updated django url/path syntax

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,9 @@
 # encoding: utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from django.conf.urls import include, url
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    path("admin/", admin.site.urls),
 ]
